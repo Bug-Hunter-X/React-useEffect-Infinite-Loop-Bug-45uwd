20b22829b900f77e8db6a80a0d78f1937deaf664
BugHunter-X <askzenai@gmail.com>
2025-01-25 01:58:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This will cause an infinite loop because the effect depends on count,
+    // which is updated by the effect itself.
+    setCount(count + 1);
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+    </div>
+  );
+}
+```
